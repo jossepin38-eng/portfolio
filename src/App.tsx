72b872c2b5035cee611210160668b0d9d6f4b6d6
jossepin38-eng <jossepin38@gmail.com>
2026-04-09 16:46:32 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -60,12 +60,22 @@ export default function App() {
          <PageTransition key="home">
            
     <Home setGnbTheme={setGnbTheme} />
- <div className="relative h-[600px] overflow-hidden">
-  <LightRays />
-
-  <div className="relative z-10">
-    {/* 기존 콘텐츠 */}
-  </div>
+<div style={{ width: '100%', height: '600px', position: 'relative' }}>
+  <LightRays
+    raysOrigin="top-center"
+    raysColor="#ffffff"
+    raysSpeed={1}
+    lightSpread={0.5}
+    rayLength={3}
+    followMouse={true}
+    mouseInfluence={0.1}
+    noiseAmount={0}
+    distortion={0}
+    className="custom-rays"
+    pulsating={false}
+    fadeDistance={1}
+    saturation={1}
+/>
 </div>
           </PageTransition>
         )}
